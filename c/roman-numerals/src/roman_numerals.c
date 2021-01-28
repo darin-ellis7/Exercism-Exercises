853c7078b7ad0c9a792155db33b49b283a0ed82a
darin-ellis7 <darin.ellis@geappliances.com>
2021-01-27 20:58:37 -0500

    Created the arabic_digit_to_roman_digits function to begin work on a generic function for each digit (V, X, etc), since there is a clear pattern developing

--- c/roman-numerals/src/roman_numerals.c
+++ c/roman-numerals/src/roman_numerals.c
@@ -1,31 +1,73 @@
 #include "roman_numerals.h"
 #include <stdlib.h>
 
+char *arabic_digit_to_roman_digits(char *romanNumber, unsigned int arabicNumber)
+{
+    unsigned int difference;
+
+    //first three
+    if(4 > arabicNumber)
+    {
+        difference = 4 - arabicNumber;
+        for (unsigned int i = 0; i < 4-difference; i++)
+        {
+            romanNumber[i] = 'I';
+        }
+    }
+    //one below five
+    else if(4 == arabicNumber)
+    {
+        romanNumber[0] = 'I';
+        romanNumber[1] = 'V';
+    }
+    //even with five and up to three above it
+    else if(4 < arabicNumber && 9 > arabicNumber)
+    {
+        romanNumber[0] = 'V';
+        for(unsigned int i = 0; i < arabicNumber - 5; i++)
+         {
+             romanNumber[i+1] = 'I';
+         }
+    }
+    return romanNumber;
+}
+
 char *to_roman_numeral(unsigned int number)
 {
+    //Roman digit key:
+    // I = 1
+    // V = 5
+    // X = 10
+    // L = 50
+    // C = 100
+    // D = 500
+    // M = 1000
+
     char *romanNumeral = calloc(15, sizeof(char));
     
-    if(number <= 3)
-    {
-        for(unsigned int i = 0; i < number; i++)
-        {
-            romanNumeral[i] = 'I';
-        }
-    }
-    else if(number == 4)
-    {
-        romanNumeral[0] = 'I';
-        romanNumeral[1] = 'V';
-    }
-    else if(number >= 5 && number <= 8)
-    {
-        romanNumeral[0] = 'V';
-        for(unsigned int i = 1; i < number - 4; i++)
-        {
-            romanNumeral[i] = 'I';
-        }
-    }
-    else if(number == 9)
+    romanNumeral = arabic_digit_to_roman_digits(romanNumeral, number);
+
+    // if(number <= 3)
+    // {
+    //     for(unsigned int i = 0; i < number; i++)
+    //     {
+    //         romanNumeral[i] = 'I';
+    //     }
+    // }
+    // else if(number == 4)
+    // {
+    //     romanNumeral[0] = 'I';
+    //     romanNumeral[1] = 'V';
+    // }
+    // else if(number >= 5 && number <= 8)
+    // {
+    //     romanNumeral[0] = 'V';
+    //     for(unsigned int i = 0; i < number - 5; i++)
+    //     {
+    //         romanNumeral[i+1] = 'I';
+    //     }
+    // }
+    if(number == 9)
     {
         romanNumeral[0] = 'I';
         romanNumeral[1] = 'X';
--- c/roman-numerals/src/roman_numerals.h
+++ c/roman-numerals/src/roman_numerals.h
@@ -2,5 +2,6 @@
 #define ROMAN_NUMERALS_H
 
 char *to_roman_numeral(unsigned int number);
+char *arabic_digit_to_roman_digits(char *romanNumber, unsigned int arabicNumber);
 
 #endif
